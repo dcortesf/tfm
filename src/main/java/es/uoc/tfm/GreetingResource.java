927733c9c90297df6d289606447d48ad894ced37
Daniel Cortés Fernández <daniel.cortes.fernandez@bbva.com>
2023-04-11 21:02:22 +0200

    PoC simple de métricas

--- src/main/java/es/uoc/tfm/GreetingResource.java
+++ src/main/java/es/uoc/tfm/GreetingResource.java
@@ -5,12 +5,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
 @Path("/hello")
+@Counted(name = "performedChecks", description = "How many primality checks have been performed.")
+@Timed(name = "checksTimer", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Metricas activadas";
     }
 }
